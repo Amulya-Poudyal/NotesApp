a955d158260191fe8b4b10f210ddbc410b8d8c2e
Amulya-Poudyal <amulyap60@gmail.com>
2026-05-08 14:11:59 +0545

    added authentication for users

--- validators/noteValidator.js
+++ validators/noteValidator.js
@@ -4,4 +4,4 @@ import { z } from 'zod';
 export const noteSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters").max(100),
   content: z.string().min(1, "Content cannot be empty"),
-});
+}); 
