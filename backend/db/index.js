e4f20a2a32468753ac18fdc190a75282ac27deaa
Amulya-Poudyal <amulyap60@gmail.com>
2026-05-10 13:53:12 +0545

    Made some necessary changes

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,30 +1,22 @@
-import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2/promise';
-import 'dotenv/config'
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import "dotenv/config";
 
-// 1. Import all your separate table files
-import * as notesSchema from './schema/notes.js';
-import * as usersSchema from './schema/users.js';
-import * as foldersSchema from './schema/folders.js';
-import * as tagsSchema from './schema/tags.js';
+import * as notesSchema from "./schema/notes.js";
+import * as usersSchema from "./schema/users.js";
+import * as foldersSchema from "./schema/folders.js";
+import * as tagsSchema from "./schema/tags.js";
+import * as relations from "./relations.js";
 
-// 2. Import your central relations file
-import * as relations from './relations.js';
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
-const connection = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "notesapp",
-});
-
-// 3. THE HOOKUP: Spread everything into the schema object
-export const db = drizzle(connection, { 
-  schema: { 
-    ...notesSchema, 
-    ...usersSchema, 
-    ...foldersSchema, 
-    ...tagsSchema, 
-    ...relations 
-  },mode:"default"
+export const db = drizzle(pool, {
+  schema: {
+    ...notesSchema,
+    ...usersSchema,
+    ...foldersSchema,
+    ...tagsSchema,
+    ...relations,
+  },
+  mode: "default",
 });
